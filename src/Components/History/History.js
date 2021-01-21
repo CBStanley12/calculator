37b143849a55da909970b59e2c203b2a9eda9568
Christopher Stanley <chris@cbstanley.dev>
2021-01-21 13:46:04 -0700

    fix: scroll behavior of history component and highlight last entry

--- src/Components/History/History.js
+++ src/Components/History/History.js
@@ -6,11 +6,9 @@ function History({ list, state, toggle, clear }) {
 		<aside className="layout-history history" data-state={`is-${state}`}>
 			<Button id="toggle" value="Close" cls="history" click={toggle} />
 
-			<div className={(list.length === 0) ? "history_list empty" : "history_list"}>
-				<ul>
-					{(list.length === 0) ? "No History" : list.map((entry, key) => FormatEntry(key, entry))}
-				</ul>
-			</div>
+			<ul className={(list.length === 0) ? "history_list empty" : "history_list"}>
+				{(list.length === 0) ? "No History" : list.map((entry, key) => FormatEntry(key, entry))}
+			</ul>
 
 			<footer className="history_footer">
 				<Button id="clear-history" value="Clear History" cls="clear" click={clear} />
@@ -21,7 +19,7 @@ function History({ list, state, toggle, clear }) {
 
 function FormatEntry(id, [equation, value]) {
 	return (
-		<li id={id} className="list-entry">
+		<li key={id} id={id} className="list-entry">
 			<p className="list-entry_equation">{equation}</p>
 			<p className="list-entry_value">{value}</p>
 		</li>
